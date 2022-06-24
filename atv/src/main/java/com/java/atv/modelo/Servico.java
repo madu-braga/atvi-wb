b33f073458f0e26a885b72492c511a362ac0ed83
Maria Eduarda Macedo Braga <78921099+madu-braga@users.noreply.github.com>
2022-06-24 01:35:55 -0300

    Update Servico.java

--- atv/src/main/java/com/java/atv/modelo/Servico.java
+++ atv/src/main/java/com/java/atv/modelo/Servico.java
@@ -1,5 +1,13 @@
 package com.java.atv.modelo;
 
-public abstract class Servico {
+public class Servico {
 	public String nome;
+	public Number valor;
+	public Servico(String nome, Number valor) {
+		this.nome = nome;
+		this.valor = valor;
+	}
+	public String getNome() {
+		return nome;
+	}
 }
