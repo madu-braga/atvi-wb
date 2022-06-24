1245010a6f65c17460cbcd7b7648b8bf83fe17b5
Maria Eduarda Macedo Braga <78921099+madu-braga@users.noreply.github.com>
2022-06-24 00:27:29 -0300

    Add files via upload

---
+++ atv/src/main/java/com/java/atv/AtvApplication.java
@@ -0,0 +1,13 @@
+package com.java.atv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvApplication.class, args);
+	}
+
+}
