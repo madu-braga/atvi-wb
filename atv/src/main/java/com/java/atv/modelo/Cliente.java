f8a4eb356745aad11c1ddcefca857396f4e2bc5f
Maria Eduarda Macedo Braga <78921099+madu-braga@users.noreply.github.com>
2022-06-24 01:27:16 -0300

    Update Cliente.java

--- atv/src/main/java/com/java/atv/modelo/Cliente.java
+++ atv/src/main/java/com/java/atv/modelo/Cliente.java
@@ -5,19 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cliente {
+	public String genero;
 	public String nome;
-	public String nomeSocial;
 	private CPF cpf;
-	private List<RG> rgs;
+	private RG rgs;
 	private LocalDate dataCadastro;
 	private List<Telefone> telefones;
 	private List<Produto> produtosConsumidos;
 	private List<Servico> servicosConsumidos;
-	public Cliente(String nome, String nomeSocial, CPF cpf) {
+	public Cliente(String nome,String genero, CPF cpf, RG rg) {
+		this.genero = genero;
 		this.nome = nome;
-		this.nomeSocial = nomeSocial;
 		this.cpf = cpf;
-		this.rgs = new ArrayList<>();
+		this.rgs = rg;
 		this.dataCadastro = LocalDate.now();
 		this.telefones = new ArrayList<>();
 		this.produtosConsumidos = new ArrayList<>();
@@ -26,11 +26,14 @@ public class Cliente {
 	public CPF getCpf() {
 		return cpf;
 	}
-	public List<RG> getRgs() {
+	public RG getRgs() {
 		return rgs;
 	}
 	public LocalDate getDataCadastro() {
 		return dataCadastro;
+	}
+	public void setTelefones(List<Telefone> telefonesNovos) {
+		telefones = telefonesNovos;
 	}
 	public List<Telefone> getTelefones() {
 		return telefones;
